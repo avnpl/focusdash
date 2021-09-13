1339d1c3f1ec1f16dc731f2f3eb010332154ed2e
Alvin Pauly <alvinpaul1311@gmail.com>
2021-09-13 12:45:57 +0530

    Background changes everyday without overflowing

--- bg.js
+++ bg.js
@@ -1,32 +1,18 @@
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-var imgToday = new Date();
-var imgDate = imgToday.getDate();
+const body = document.querySelector("body");
+const date = new Date();
+const day = date.getDate();
 
-function changeBg() {
-  var apiUrl = `https://pixabay.com/api/?key=18503274-e0fb94028e76bf5c37d20f2b3&q=landscape&orientation=horizontal&min_width=1920&min_height=1080&page=${randomNumber(
-    1,
-    26
-  )}`;
-  fetch(apiUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var imgSrc = data.hits[randomNumber(1, 10)];
+// body.style.backgroundImage = `img/${day}.jpg`;
 
-      //console.log(imgSrc);
-      document.body.style.background =
-        "url(" +
-        ` ${imgSrc.largeImageURL} ` +
-        ")" +
-        " no-repeat" +
-        " center" +
-        " center" +
-        " fixed";
-      document.body.style.backgroundSize = "cover";
-    });
+function paintImage(imgNumber) {
+  const image = new Image();
+  image.src = `img/${imgNumber}.jpg`;
+  image.classList.add("bgImage");
+  body.appendChild(image);
 }
 
-changeBg();
+function init() {
+  paintImage(day);
+}
+
+init();
